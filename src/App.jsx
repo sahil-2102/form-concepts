e5619556aefd1ef7634f08692251352f43f38a2a
Sahil <sk4386840@gmail.com>
2025-06-26 12:14:14 +0530

    update the component name from  from to Signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Form from "./components/Form"
+import Signup from "./pages/Signup"
 
 const App = () => {
   return (
     <div className="h-screen bg-gradient-to-r from-gray-700 to-gray-400 flex items-center justify-center">
-      <Form/>
+      <Signup/>
     </div>
   )
 }
