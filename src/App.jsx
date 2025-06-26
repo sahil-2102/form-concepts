bdb48814fb8116226256dabc874dccd8fb8f640e
Sahil <sk4386840@gmail.com>
2025-06-26 12:17:36 +0530

    install and initialize react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-
+import {Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
     <div className="h-screen bg-gradient-to-r from-gray-700 to-gray-400 flex items-center justify-center">
-      <Signup/>
+      <Routes>
+        <Route path="/" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   )
 }
